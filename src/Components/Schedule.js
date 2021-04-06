155aea48d4783fc4e68d7a88a26f7d5726dcaae4
Daniil Chistyakov <4i.danila@gmail.com>
2021-04-06 17:58:35 +0300

    UI Improved

--- src/Components/Schedule.js
+++ src/Components/Schedule.js
@@ -12,44 +12,23 @@ const Schedule = () => {
         Fetch();
         setInterval(Fetch, 180000)
     }, [])
-    if (info?.current_playlist.name === 'Дэн Климов') {
-        return (
-            <section className={style.schedule_section}>
-                <div className={style.now_onair}>
-                    <h2>У микрофона:</h2>
-                    <div className={style.dj_block}>
-                        <h3 className={style.dj_name}>Дэн Климов</h3>
-                        <h3 className={style.dj_time}>{info?.current_playlist.hour + 2}:00&nbsp;&nbsp;&gt;&nbsp;&nbsp;{info?.current_playlist.end_time + 2}:00</h3>
-                    </div>
+    return (
+        <section className={style.schedule_section}>
+            <div className={style.now_onair}>
+                <h2>У микрофона:</h2>
+                <div className={style.dj_block}>
+                    <h3 className={style.dj_name}>{info?.current_playlist.name}</h3>
+                    <h3 className={style.dj_time}>{info?.current_playlist.hour + 2}:00&nbsp;&nbsp;&gt;&nbsp;&nbsp;{info?.current_playlist.end_time + 2}:00</h3>
                 </div>
-                <div className={style.next_onair}>
-                    <h2>Далее в эфире:</h2>
-                    <h3>{info?.next_playlist.name}</h3>
-                    <p>{info?.next_playlist.description}</p>
-                    <p>{info?.next_playlist.hour + 2}:00 — {info?.next_playlist.end_time + 2}:00</p>
-                </div>
-            </section>
-        )
-    }
-    else {
-        return (
-            <section className={style.schedule_section}>
-                <div className={style.now_onair}>
-                    <h2>Сейчас в эфире:</h2>
-                    <h3>{info?.current_playlist.name}</h3>
-                    <p>{info?.current_playlist.description}</p>
-                    <p>{info?.current_playlist.hour + 2}:00 — {info?.current_playlist.end_time + 2}:00</p>
-
-                </div>
-                <div className={style.next_onair}>
-                    <h2>Далее в эфире:</h2>
-                    <h3>{info?.next_playlist.name}</h3>
-                    <p>{info?.next_playlist.description}</p>
-                    <p>{info?.next_playlist.hour + 2}:00 — {info?.next_playlist.end_time + 2}:00</p>
-                </div>
-            </section>
-        )
-    }
+            </div>
+            <div className={style.next_onair}>
+                <h2>Далее в эфире:</h2>
+                <h3>{info?.next_playlist.name}</h3>
+                <p>{info?.next_playlist.description}</p>
+                <p>{info?.next_playlist.hour + 2}:00 — {info?.next_playlist.end_time + 2}:00</p>
+            </div>
+        </section>
+    )
 }
 
 export default Schedule
