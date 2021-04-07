bdf5881a637ed39118896e1b78c39eda26563dd1
Daniil Chistyakov <4i.danila@gmail.com>
2021-04-07 08:43:38 +0300

    Recognition improved

--- src/Components/Lastsongs.js
+++ src/Components/Lastsongs.js
@@ -39,7 +39,7 @@ const Lastsongs = () => {
             setResults(results);
         }
         Fetch();
-        setInterval(Fetch, 15000);
+        setInterval(Fetch, 50000);
     }, [])
     return (
         <section className={style.lastsongs}>
