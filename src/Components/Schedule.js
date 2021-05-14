b5ac3bc3342b8eabea2d7405dfeb6ce35fe79f45
Daniil Chistyakov <4i.danila@gmail.com>
2021-05-14 23:05:09 +0300

    Time bug fixed

--- src/Components/Schedule.js
+++ src/Components/Schedule.js
@@ -18,14 +18,14 @@ const Schedule = () => {
                 <h2>У микрофона:</h2>
                 <div className={style.dj_block}>
                     <h3 className={style.dj_name}>{info?.current_playlist.name}</h3>
-                    <h3 className={style.dj_time}>{info?.current_playlist.hour + 2}:00&nbsp;&nbsp;&gt;&nbsp;&nbsp;{info?.current_playlist.end_time + 2}:00</h3>
+                    <h3 className={style.dj_time}>{info?.current_playlist.hour + 1}:00&nbsp;&nbsp;&gt;&nbsp;&nbsp;{info?.current_playlist.end_time + 1}:00</h3>
                 </div>
             </div>
             <div className={style.next_onair}>
                 <h2>Далее в эфире:</h2>
                 <h3>{info?.next_playlist.name}</h3>
                 <p>{info?.next_playlist.description}</p>
-                <p>{info?.next_playlist.hour + 2}:00 — {info?.next_playlist.end_time + 2}:00</p>
+                <p>{info?.next_playlist.hour + 1}:00 — {info?.next_playlist.end_time + 1}:00</p>
             </div>
         </section>
     )
